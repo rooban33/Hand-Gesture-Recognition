efdbeabca645a327de02901847a7586ab9e07038
rooban33 <95079619+rooban33@users.noreply.github.com>
2023-09-11 18:04:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def stop_gesture_recognition():
         return jsonify({'message': 'Gesture recognition not running'})
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
